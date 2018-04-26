20c3347d8660344c001aabc119ef6d5c80ea7fa3
wynnechen <wynnechen@tencent.com>
2018-04-26 20:44:24 +0800

    modify out name

--- pointlight/run.sh
+++ pointlight/run.sh
@@ -1,2 +1,2 @@
-clang++ -std=c++11 -stdlib=libc++ -lglfw3 -framework CoreVideo -framework OpenGL -framework IOKit -framework Cocoa -framework Carbon main.cpp ../Shader.cpp ../glad.c -o lightmap.out
-./lightmap.out
+clang++ -std=c++11 -stdlib=libc++ -lglfw3 -framework CoreVideo -framework OpenGL -framework IOKit -framework Cocoa -framework Carbon main.cpp ../Shader.cpp ../glad.c -o pointlight.out
+./pointlight.out
