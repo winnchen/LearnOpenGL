8478ad7929d7865e439794c1c46257cfc167a7a2
wynnechen <wynnechen@tencent.com>
2018-04-26 15:45:15 +0800

    add direction light

---
+++ directionlight/obj_fragment_shader.fs
@@ -0,0 +1,46 @@
+#version 330 core
+
+struct Material
+{
+    sampler2D diffuse;
+    sampler2D specular; 
+    float shininess;
+};
+
+struct Light
+{
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 Pos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 color;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    //ambient
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    
+    //diffuse
+    vec3 normal = normalize(Normal);
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * (vec3(texture(material.diffuse, TexCoords)));
+
+    //specular
+    vec3 viewDir = normalize(viewPos - Pos);
+    vec3 reflectDir = reflect(-lightDir, normal); 
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * (vec3(texture(material.specular, TexCoords)));
+    
+    color = vec4(ambient + diffuse + specular, 1.0);
+}
